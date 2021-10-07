569f4591d312192d1fce3f89c3d0a301f81a647a
Camila <cssmasterti@gmail.com>
2021-10-07 16:46:41 -0300

    incluindo ajustes

---
+++ src/js/menu.js
@@ -0,0 +1,10 @@
+
+  let menu = document.querySelectorAll('.menu-hamburger__hamburger')
+
+  if(menu.length){
+   
+  }
+
+  menu[0].addEventListener("click", function(i){
+    console.log('clicou')
+  });
