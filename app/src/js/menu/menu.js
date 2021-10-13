9928c4658740d68449f472d705fedc3d8601239d
Camila <cssmasterti@gmail.com>
2021-10-13 17:59:12 -0300

    ajustes

--- app/src/js/menu/menu.js
+++ app/src/js/menu/menu.js
@@ -2,5 +2,6 @@ let menu = document.querySelectorAll('.menu-hamburger__hamburger');
 let list = document.querySelectorAll('.menu-hamburger__hamburger-list')
 
   menu[0].addEventListener("click", function(i){
-    list.classList.add('active')
+    menu[0].classList.add('active')
+    list[0].classList.add('active')
   });
